9f6985a16029f5772748b836b96ae098cd39e056
chendeji <781571323@qq.com>
2025-12-07 23:31:27 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar.tsx";
+import Hero from "./components/Hero.tsx";
+import Highlights from "./components/Highlights.tsx";
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+};
+
+export default App;
